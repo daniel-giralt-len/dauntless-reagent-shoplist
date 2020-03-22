c8b6e058dcae790ab951413c7dd11d4d19fc6da6
daniel-giralt-len <daniel.giralt.len@gmail.com>
2020-03-22 20:51:27 +0100

    add icon to every item

--- src/components/craftableItem/index.js
+++ src/components/craftableItem/index.js
@@ -1,20 +1,39 @@
 import styled from 'styled-components'
 import IntegerRange from '../integerRange'
 import ReagentList from '../reagentList'
+import filterIcons from '../../assets/typesToIcons.json'
+
+const ItemTypeIcon = styled.img`
+  width: 1em;
+`
 
 const ListItemWrapper = styled.div`
   display: grid;
   grid-template-areas:
-    "name"
-    "current-level";
+    "current-level"
+    "name";
   border: 1px solid black;
+`
+
+const TitleWrapper = styled.h2`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  grid-area: name;
+  font-size: 1.1em;
 `
 
 const CraftableItem = ({item, onItemLevelChange}) => {
   const {name, type, partType, remainingReagents, currentLevelIndex, availableLevels} = item
   const onSliderChange = newLevel => onItemLevelChange({name, levelIndex: newLevel})
 	return (<ListItemWrapper>
-    <div style={{gridArea: 'name'}}>{name}, {type} {partType && `(${partType})`}</div>
+    <TitleWrapper>
+      <div style={{gridArea: 'name'}}>{name}</div>
+      <ItemTypeIcon
+        src={filterIcons[type]}
+        alt={partType ? `${type} (${partType})` : type}
+      />
+    </TitleWrapper>
     <div style={{gridArea: 'current-level'}}>
       <IntegerRange 
         index={currentLevelIndex}
